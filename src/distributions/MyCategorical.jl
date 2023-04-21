14166f685820ff2d168e9657d6ffe055bd138686
Stefan Manolache <stefanmanol@gmail.com>
2023-04-21 22:06:11 +0100

    processes

--- src/distributions/MyCategorical.jl
+++ src/distributions/MyCategorical.jl
@@ -1,15 +1,44 @@
-using Distributions
+using Distributions, Random
+using PDMats
+using LogExpFunctions, LinearAlgebra
+using Plots
+using DistributionsAD
+using Bijectors
+using Statistics
 
-struct MyCategorical{V} <: ContinuousDistribution
-    values::AbstractVector{V}
-    value_ids::Dict{V, Integer}
-    c::Categorical
+import Base: length, eltype, show, size
+import Distributions: _logpdf, _logpdf!, mean, _rand!
+
+struct MyCategorical <: ContinuousUnivariateDistribution
+    raw_cat::Categorical
     lp::AbstractVector{<:Real}
 end
 
-function MyCategorical(values::AbstractVector, lp::AbstractVector{<:Real})
-
-    MyCategorical(Categorical(exp.(lp)))
+function MyCategorical(p::AbstractVector{<:Real})
+    if sum(p) != 1
+        p = normalize(p)
+        @warn "Probabilities in categorical construction do not sum up to 1"
+    end
+    MyCategorical(Categorical(p), log(p))
 end
 
-logpdf(d::MyCategorical) = lp
+logpdf(d::MyCategorical) = d.lp
+
+
+# Domain Dimension
+Base.length(wn::WrappedNormal) = length(wn.𝛷)
+
+# Domain field type
+Base.eltype(wn::WrappedNormal) = eltype(wn.𝛷)
+
+# __________________________________________________________________________________________
+# Distribution Methods
+
+function _rand!(rng::AbstractRNG, d::MyCategorical, x::AbstractVecOrMat{<: Real})
+    x .= _rand!(rng, d.raw_cat, x')
+    return x
+end
+
+function _logpdf(d::MyCategorical, x::AbstractVector{<: Real})
+    return logsumexp(logpdf(wn.𝛷, cmod.(x) .+ wn.𝕃))
+end
