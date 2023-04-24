5e228f1f26431b224416b1c2f9a15effe4c79dce
Stefan Manolache <stefanmanol@gmail.com>
2023-04-24 20:32:40 +0100

    finished general tkf92 model

--- src/distributions/EvolHMM.jl
+++ src/distributions/EvolHMM.jl
@@ -1,13 +1,20 @@
 
 
-function forward!(α, A, observations)
+function forward!(α::AbstractArray{<:Real}, model::TKF92,
+                  jointlogpdf::AbstractArray{<:Real},
+                  statlogpdfs::AbstractArray{AbstractVector{<:Real}};
+                  known_ancestor=true)
+    D = num_descendants(model)
+
     α = fill!(α, -Inf)
     # Initial state
-    α[2, 2, START] = 0
+    α[START, zeros(D)] = 0
+    grid = Iterators.product(axes(α)[2:end]...)
 
+    state_ids = proper_state_ids(model)
     # Recursion
-    for indices ∈ grid, s ∈ states
-        domino = onehot[s]
+    for indices ∈ grid, s ∈ state_ids
+        domino = value(s)
         prev_indices = _prev_indices(indices, domino)
         α[s, indices...] = _lp(domino, indices, observations) + logsumexp([A[q, s] + α[q, (indices - domino)...]])
     end
