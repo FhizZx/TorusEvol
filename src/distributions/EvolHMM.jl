2beb654f09295513ebe19019a15fd7ba055f5091
Stefan Manolache <stefanmanol@gmail.com>
2023-04-24 23:00:38 +0100

    evolhmm

--- src/distributions/EvolHMM.jl
+++ src/distributions/EvolHMM.jl
@@ -1,26 +1,31 @@
 
 
 function forward!(α::AbstractArray{<:Real}, model::TKF92,
-                  jointlogpdf::AbstractArray{<:Real},
-                  statlogpdfs::AbstractArray{AbstractVector{<:Real}};
-                  known_ancestor=true)
+                  emission_lps::AbstractArray{<:Real};
+                  known_ancestor=false)
     D = num_descendants(model)
 
     α = fill!(α, -Inf)
     # Initial state
-    α[START, zeros(D)] = 0
+    α[START, ones(D)] = 0
     grid = Iterators.product(axes(α)[2:end]...)
+    end_corner = 1 .+ size(α)
 
-    state_ids = proper_state_ids(model)
+    state_ids = state_ids(model)
+    values = known_ancestor ? values(model) : descendant_values(model)
+    #A = known_ancestor ? transmat(model) :
+
     # Recursion
     for indices ∈ grid, s ∈ state_ids
-        domino = value(s)
-        prev_indices = _prev_indices(indices, domino)
-        α[s, indices...] = _lp(domino, indices, observations) + logsumexp([A[q, s] + α[q, (indices - domino)...]])
+        if all(indices .+ 1 .>= s)
+            state = values[s]
+            @. obs_ind = ifelse(state == 1, indices, end_corner)
+            curr_ind = 1 .+ indices
+            prev_ind = curr_ind .- state
+            α[s, curr_ind...] = emission_lps[obs_ind...] +
+                                logsumexp([A[:, s] .+ α[:, prev_ind...]])
+        end
     end
 
-    # Finally need to enter the END state to complete the chain
-    logp = logsumexp([α[s, end_corner...] + A[s, END] for s in states])
-
-    return logp
+    return α
 end
