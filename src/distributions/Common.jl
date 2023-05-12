ccb98cc53a6b6c8efe9697187d27dbc6a95038f2
Stefan Manolache <stefanmanol@gmail.com>
2023-05-12 17:09:59 +0100

    ancestor sampling

--- src/distributions/Common.jl
+++ src/distributions/Common.jl
@@ -2,27 +2,6 @@ using Distributions
 
 import Distributions: logpdf
 
-#TODO - implement these as recipes for Plots.jl
-
-# Plot heatmap of the density over 𝕋²
-function plotpdf(d::ContinuousDistribution; step=π/100)
-    ticks = (-π):step:π
-    if length(d) == 2
-        grid = hcat([[j, i] for i in ticks, j in ticks]...)
-        z = reshape(pdf(d, grid), length(ticks), :)
-        heatmap(ticks, ticks, z, size=(400, 400), title="Density",
-                xlabel="ϕ angles", ylabel="ψ angles")
-    else
-        throw("plotting not implemented for d != 2")
-    end
-end
-
-# Scatter plot of samples from wn
-function plotsamples(d::ContinuousDistribution, n_samples)
-    samples = rand(d, n_samples)
-    scatter(eachrow(samples)...,size=(400,400),
-            title="Samples", label="", alpha=0.3)
-end
 
 # Fix for categorical logpdf
 Distributions.logpdf(d::Categorical, x::AbstractArray{<:Real}) = logpdf.(d, x)
